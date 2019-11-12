3fe10958ff15ab9bb91626a25ae2270436ea8813
Administrator <carlao.linux@gmail.com>
2019-11-12 16:27:43 -0300

    Terminar EnrollmentController

---
+++ src/app/controllers/EnrollmentController.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+import Enrollment from '../models/Enrollment';
+import Student from '../models/Student';
+
+class EnrollmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.date().required(),
+      end_date: Yup.date().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { start_date, plan_id } = req.body;
+
+    return res.send();
+  }
+}
+
+export default new EnrollmentController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import EnrollmentController from './app/controllers/EnrollmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,7 @@ routes.post('/plan', PlanController.store);
 routes.get('/plan/', PlanController.index);
 routes.put('/plan/:id', PlanController.update);
 routes.delete('/plan/:id', PlanController.delete);
+
+routes.post('/enrollment', EnrollmentController.store);
 
 export default routes;
